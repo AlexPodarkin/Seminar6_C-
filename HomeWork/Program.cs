5408f8b07f3be63f5647ef9286949d396414ae1c
Aleksandr <alexchembarov@gmail.com>
2022-10-04 17:16:12 +0400

    HomeWork

---
+++ HomeWork/Program.cs
@@ -0,0 +1,78 @@
+﻿Console.Clear();
+Console.Write("Введите номер задачи(1-3):");
+int nom = Convert.ToInt32(Console.ReadLine());
+switch (nom)
+{
+    case 1:
+        Console.WriteLine("Алгоритм считаетсколько чисел больше 0 ввёл пользователь.");
+        int count = 0;
+        Console.Write("Введите размер массива:");
+        int size = Convert.ToInt32(Console.ReadLine());    // пользователь задает размер массива
+        int[] arrayP = new int[size];                      // алгоритм его создает
+        for (int i = 0; i < arrayP.Length; i++)            //цикл наполнения массива 
+        {
+            Console.Write($"Введите {i + 1} число массива:");
+            arrayP[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("[ " + string.Join(", ", arrayP) + " ]");
+        for (int i = 0; i < arrayP.Length; i++)
+        {
+            if (arrayP[i] > 0) count += 1;
+        }
+        Console.WriteLine($"в данном масиве {count} элемента(ов) больше нуля !");
+        break;
+
+    case 2:
+        Console.WriteLine("Алгоритм нахождения точки пересечния прямых.");
+        Console.Write($"Введите b1 первую координату:");
+        double b1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Введите k1 вторую координату:");
+        double k1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Введите b2 третью координату:");
+        double b2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Введите k2 четвертую координату:");
+        double k2 = Convert.ToInt32(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+
+        double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+        Console.WriteLine($"Точка пересечения прямых имеет координаты: ({x}, {y})");
+        break;
+    case 3:
+        Console.WriteLine("Алгоритм который сдвигает массив.");
+        Console.Write("Введите размер массива:");
+        int size3 = Convert.ToInt32(Console.ReadLine());
+        int[] arrayI = new int[size3];
+        for (int i = 0; i < arrayI.Length; i++)
+        {
+            Console.Write($"Введите {i + 1} число массива:");
+            arrayI[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("[ " + string.Join(", ", arrayI) + " ]");
+
+        if (arrayI.Length > 1)
+        {
+            int tmp = arrayI[arrayI.Length - 1];
+
+            for (int i = arrayI.Length - 1; i != 0; --i) { arrayI[i] = arrayI[i - 1]; }
+
+            arrayI[0] = tmp;
+        }
+        Console.WriteLine("Сдвиг вправо [ " + string.Join(", ", arrayI) + " ]");
+        break;
+}
+
+/* 
+ */
+
+/* 
+ */
+
+
+
+
+
+
+/* */
+
--- lesson/Program.cs
+++ lesson/Program.cs
@@ -1,2 +1,139 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+/* int[] array = FillArray(5, 1, 10);
+Console.WriteLine("[" + String.Join(",", array) + "]");
+int[] FillArray(int size, int min, int max) 
+{
+    int[] filledArray = new int[size];
+
+    for (int i = 0; i < filledArray.Length; i++)
+    {
+        filledArray[i] = new Random().Next(min, max + 1);
+    }
+    return filledArray;
+}
+ */
+
+/* int[] array = FillArray(5, 1, 10);
+
+Console.WriteLine("Входной массив:" + "[ " + String.Join(",", array) + " ]");
+
+Console.WriteLine("Перевёрнутый массив: " + "[ " + String.Join(",", ReverseArray(array)) + " ]");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] filledArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        filledArray[i] = new Random().Next(min, max + 1);
+    }
+    return filledArray;
+}
+
+int[] ReverseArray(int[] inputArray)
+{
+    int[] reverseArray = new int[inputArray.Length];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        reverseArray[i] = inputArray[inputArray.Length - 1 - i];
+    }
+    return reverseArray;
+} */
+
+
+/* Console.Write($"введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 < (num2 + num3) && num2 < (num1 + num3) && num3 < (num2 + num1))
+    Console.WriteLine($"Такой треугольник возможен");
+else Console.WriteLine($"Такой треугольник НЕ возможен"); */
+
+/* Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. */
+
+
+/* int[] array = FillArray(10, 1, 10);
+
+Console.WriteLine("Входной массив:" + "[ " + String.Join(",", array) + " ]");
+
+Console.WriteLine("Копия массив: " + " [ " + String.Join(",", RevArray(array)) + " ]");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] filledArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        filledArray[i] = new Random().Next(min, max + 1);
+    }
+    return filledArray;
+}
+
+int[] RevArray(int[] inputArray)
+{
+    int[] revArray = new int[inputArray.Length];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        revArray[i] = inputArray[i];
+    }
+    return revArray;
+} */
+
+
+/* Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"В двоичной системе: {ConvertTo2(num)}");
+
+string ConvertTo2(int num_local)
+{
+    string s = "";
+    while (num_local > 0)
+    {
+        s = ((num_local % 2 == 0) ? "0" : "1") + s;
+        num_local /= 2;
+    }
+    if (s == "") s = "0";
+    return s;
+} */
+
+
+/* Console.WriteLine();
+Console.Write("Введите число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int[] array = new int[num2];
+array[0] = 0;
+array[1] = 1;
+
+for (int i = 2; i < num2; i++)
+{
+    array[i] = array[i-1] + array[i-2];
+}
+
+Console.WriteLine("Фибоначи: " + String.Join(", ", array)); */
+
+
+/* Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"В двоичной системе: {ConvertTo2(num)}");
+
+string ConvertTo2(int num_local)
+{
+    string s = "";
+    while (num_local > 0)
+    {
+        s = ((num_local % 2 == 0) ? "0" : "1") + s;
+        num_local /= 2;
+    }
+    if (s == "") s = "0";
+    return s;
+} */
+
+
